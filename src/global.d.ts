6bbcfcfd91f5f077b94bab8007d3db5a4dade468
Matias Hernandez <matiasfh@gmail.com>
2021-05-12 05:40:33 -0400

    set env

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,8 @@
 /// <reference types="@sveltejs/kit" />
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+	VITE_API: string;
+	VITE_PRISMIC_TOKEN: string;
+}
